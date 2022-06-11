894ac2c7415cb655931da9e85601de859eb3d629
Saeed Nemati <sn.1361@gmail.com>
2022-06-11 07:30:26 +0430

    committed by script

--- User/Choose.jsx
+++ User/Choose.jsx
@@ -37,7 +37,7 @@ const Locales = () => {
                         let now = new Date()
                         now.setMonth(now.getMonth() + 12)
                         let host = document.location.host.split('.').reverse().splice(0, 2).reverse().join('.')
-                        localStorage.setEntity('locale', locale.key)
+                        localStorage.setItem('locale', locale.key)
                         document.location.reload()
                     }}
                 >
